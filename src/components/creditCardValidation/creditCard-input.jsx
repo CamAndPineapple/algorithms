76267aeb024a9fd49fb0091f605b319be2a1677f
Camden Rudisill <camden.rudisill@richmond.edu>
2016-03-20 19:29:54 -0700

    Add: all fields are transferring to credit card div

--- src/components/creditCardValidation/creditCard-input.jsx
+++ src/components/creditCardValidation/creditCard-input.jsx
@@ -4,6 +4,8 @@ import CreditCard from './CreditCard.jsx';
 // correct number: 4012-8888-8888-1881
 //7768768686876888
 
+// TODO: Figure out what you want to do about input highlight when clicked
+
 
 export default class CreditCardInput extends Component {
   constructor(props) {
@@ -11,7 +13,10 @@ export default class CreditCardInput extends Component {
 
     // set default number and search box text to empty string
     this.state = {
-      term: '',
+      cardNumber: '',
+      cardHolder: '',
+      expMonth: '',
+      expYear: ''
     }
 
     this.invalidNumber = false;
@@ -24,6 +29,7 @@ export default class CreditCardInput extends Component {
     this.onInputChange = this.onInputChange.bind(this);
     this.onSubmitForm = this.onSubmitForm.bind(this);
     this.onKeyDown = this.onKeyDown.bind(this);
+    this.onSelectChange = this.onSelectChange.bind(this);
   }
 
   onKeyDown(e) {
@@ -38,26 +44,38 @@ export default class CreditCardInput extends Component {
 
     //TODO try adding cardNumber to state
 
-
-
     // Update state as you type in the search box
-    this.setState({term: e.target.value});
+    // var nextState = {};
+    //     nextState[e.target.name] = e.target.value;
+    this.setState({ [e.target.name]: e.target.value });
 
-    if (this.state.term.length === 3 || this.state.term.length === 8 || this.state.term.length === 13) {
-      this.deleteKeyPress ? this.setState({term: e.target.value }) : this.setState({term: e.target.value + "-"});
+
+
+
+    if (this.state.cardNumber.length === 3 || this.state.cardNumber.length === 8 || this.state.cardNumber.length === 13) {
+      this.deleteKeyPress ? this.setState({cardNumber: e.target.value }) : this.setState({cardNumber: e.target.value + "-"});
     }
-    else if (this.state.term.length === 19 || e.target.value.length === 19) {
+    // If user enters 19 numbers or copy/paste 19 numbers
+    // into cardNumber input, submit form
+    else if (this.state.cardNumber.length === 19 || (e.target.name === "cardNumber" && e.target.value.length === 19)) {
       this.onSubmitForm(e.target.value);
-    } else if (this.state.term.length < 19) {
+    } else if (this.state.cardNumber.length < 19) {
       this.showMessage = false;
       this.invalidNumber = false;
     }
   }
 
+  onSelectChange(e) {
+    if (e.target.name === "expMonth"){
+      this.setState({ [e.target.name]: e.target.value + "/" });
+    } else {
+      this.setState({ [e.target.name]: e.target.value.slice(2) });
+    }
+
+  }
+
   onSubmitForm(e) {
-
-    this.validateCardNumber(e);
-
+      this.validateCardNumber(e);
   }
 
   validateCardNumber(number) {
@@ -83,7 +101,6 @@ export default class CreditCardInput extends Component {
       if (isNaN(cardNumber[i])) {
         this.invalidNumber = true;
         this.isNotANumber = true;
-        console.log("yes");
         return "value entered is not a number";
       } else {
         this.isNotANumber = false;
@@ -135,13 +152,50 @@ export default class CreditCardInput extends Component {
   render() {
     return (
       <div>
-        <CreditCard cardNumber={this.state.term}/>
+        <CreditCard cardNumber={this.state.cardNumber} cardHolder={this.state.cardHolder} expMonth={this.state.expMonth} expYear={this.state.expYear}/>
       <div className="error-message">{this.invalidNumber ? this.isNotANumber ? <p className="error-text">Please enter <span className="num-text">numbers</span> only </p> : <p className="error-text">Card Number is Incorrect</p> : this.showMessage ? <p className="no-error-show">Card Number is Correct</p> : <p className="no-error-transparent">Card Number is Correct</p> }</div>
       <div className="form-container">
         <form className="form">
-          <div className="form-group" id="search-container">
+          <label>Card Number: </label>
+          <div className={this.invalidNumber ? "form-group-error" : "form-group"}>
             <i className="fa fa-credit-card"></i>
-            <input className="form-input" id="search-input" maxLength={19} onKeyDown={this.onKeyDown} onChange={this.onInputChange} placeholder="4012-8888-8888-1881" value={this.state.term}/>
+            <input className="form-input" id="search-input" name="cardNumber" maxLength={19} onKeyDown={this.onKeyDown} onChange={this.onInputChange} placeholder="4012-8888-8888-1881" value={this.state.cardNumber}/>
+          </div>
+          <label>Card Holder: </label>
+          <div className="form-group">
+            <i className="fa fa-user"></i>
+            <input className="form-input" type="text" name="cardHolder" onChange={this.onInputChange} placeholder="Camden Rudisill" value={this.state.cardHolder}/>
+          </div>
+          <label>Expiration Date: </label>
+          <div className="form-select">
+              <div className="select">
+              <select name="expMonth" onChange={this.onSelectChange}>
+                <option></option>
+                <option>01</option>
+                <option>02</option>
+                <option>03</option>
+                <option>04</option>
+                <option>05</option>
+                <option>06</option>
+                <option>07</option>
+                <option>08</option>
+                <option>09</option>
+                <option>10</option>
+                <option>11</option>
+                <option>12</option>
+              </select>
+              </div>
+              <div className="select" >
+              <select name="expYear" onChange={this.onSelectChange}>
+                <option></option>
+                <option>2016</option>
+                <option>2017</option>
+                <option>2018</option>
+                <option>2019</option>
+                <option>2020</option>
+                <option>2021</option>
+              </select>
+              </div>
           </div>
         </form>
       </div>
--- src/components/creditCardValidation/creditCard.jsx
+++ src/components/creditCardValidation/creditCard.jsx
@@ -11,6 +11,11 @@ export default class CreditCard extends Component {
         <div className="credit-card">
             <h1>Camden Kredit</h1>
             <h2>{this.props.cardNumber}</h2>
+            <label className="card-holder-label">CARD HOLDER</label>
+            <h3 className="card-holder-text">{this.props.cardHolder}</h3>
+            <label className="expDate-label">VALID <br></br> THRU</label>
+            <h3 className="expMonth-text">{this.props.expMonth}</h3>
+            <h3 className="expYear-text">{this.props.expYear}</h3>
         </div>
       </div>
     );
