16e6815c64c36615589fffa6d30278f563507ab4
Camden Rudisill <camden.rudisill@richmond.edu>
2016-03-23 22:40:07 -0700

    Update: Refactored code, built helper functions for algo

--- src/components/creditCardValidation/creditCard-input.jsx
+++ src/components/creditCardValidation/creditCard-input.jsx
@@ -5,14 +5,14 @@ import CreditCard from './CreditCard.jsx';
 
 export default class CreditCardInput extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       cardNumber: '',
       cardHolder: '',
       expMonth: '',
       expYear: ''
-    }
+    };
 
     // Error flags
     this.invalidNumber = false;
@@ -33,7 +33,7 @@ export default class CreditCardInput extends Component {
       this.deleteKeyPress = true;
     } else {
       this.deleteKeyPress = false;
-    };
+    }
   }
 
   onInputChange(e) {
@@ -44,18 +44,22 @@ export default class CreditCardInput extends Component {
     var inputName = e.target.name;
 
     const deleteKeyPress = this.deleteKeyPress;
-    const maxLength = 19
+    const maxLength = 19;
 
-    var userEnteredAllNumbers = numLength === maxLength && inputName === "cardNumber";
+    var userEnteredAllNumbers = inputName === "cardNumber" &&  numLength === maxLength;
     var userPastedAllNumbers = inputName === "cardNumber" && numInput.length === maxLength;
 
     // Update state as you type in the input fields
-    this.setState({[e.target.name]: e.target.value});
+    this.setState({
+      [e.target.name]: e.target.value
+    });
 
     // Add hyphens to every group of 4 numbers entered
-    if (numLength === 3 || numLength === 8 || numLength === 13) {
-      this.setState({ cardNumber: deleteKeyPress ? numInput : numInput + "-"});
-        // If user enters 19 numbers or copy/paste 19 numbers
+    if ([3, 8, 13].indexOf(numLength) > -1) {
+      this.setState({
+        cardNumber: deleteKeyPress ? numInput : numInput + "-"
+      });
+      // If user enters 19 numbers or copy/paste 19 numbers
       // into cardNumber input, submit form;
     } else if (userEnteredAllNumbers || userPastedAllNumbers) {
       this.onSubmitForm(numInput);
@@ -67,9 +71,13 @@ export default class CreditCardInput extends Component {
 
   onSelectChange(e) {
     if (e.target.name === "expMonth") {
-      this.setState({[e.target.name]: e.target.value + "/"});
+      this.setState({
+        [e.target.name]: e.target.value + "/"
+      });
     } else {
-      this.setState({[e.target.name]: e.target.value.slice(2)});
+      this.setState({
+        [e.target.name]: e.target.value.slice(2)
+      });
     }
   }
 
@@ -80,55 +88,50 @@ export default class CreditCardInput extends Component {
   validateCardNumber(number) {
 
     var numberToArray = number.toString().split('');
-
-    var filterOutHyphen = numberToArray.filter(function(char) {
-      return char !== '-';
-    });
+    var filterOutHyphen = numberToArray.filter( char => char !== '-');
 
     // convert array of strings to numbers
-    var cardNumber = filterOutHyphen.map(function(num) {
-      return Number(num);
-    });
-
-    var checkSum = cardNumber[cardNumber.length - 1];
+    var cardNumber = filterOutHyphen.map( num => Number(num));
     var numLength = cardNumber.length;
-    var numbersToAdd = [];
-    var doubleNumber;
-    var sum;
-    var total;
 
-    for (var i = 0; i < numLength; i++) {
-      if (isNaN(cardNumber[i])) {
+    this.checkIfTypeNumber(cardNumber, numLength);
+    this.luhnAlgorithm(cardNumber, numLength);
+  }
+
+  checkIfTypeNumber(number, length) {
+    for (var i = 0; i < length; i++) {
+      if (isNaN(number[i])) {
         this.invalidNumber = true;
         this.isNotANumber = true;
         return "value entered is not a number";
       } else {
         this.isNotANumber = false;
-      };
+      }
     }
+  }
 
-    // Luhn Algorithm
-    for (var i = numLength - 1; i >= 0; i--) {
+  luhnAlgorithm(number, length) {
+
+    var checkSum = number[number.length - 1];
+    var numbersToAdd = [];
+    var doubleNumber;
+    var sum;
+    var total;
+
+    for (var i = length - 1; i >= 0; i--) {
       if (i === 15) {
         numbersToAdd.push(0);
-      } else if (i % 2 === 0 && i !== 0) {
+      } else if (i % 2 === 0) {
         // if doubled number is greater than 10
         // add the two digits together i.e. 12 === 1 + 2 === 3
-        if (cardNumber[i] * 2 - 10 >= 0) {
-          doubleNumber = (cardNumber[i] * 2) % 10 + 1;
+        if (number[i] * 2 - 10 >= 0) {
+          doubleNumber = (number[i] * 2) % 10 + 1;
           numbersToAdd.push(doubleNumber);
         } else {
-          numbersToAdd.push(cardNumber[i] * 2);
-        }
-      } else if (i === 0) {
-        if (cardNumber[i] * 2 - 10 >= 0) {
-          doubleNumber = (cardNumber[i] * 2 % 10) + 1;
-          numbersToAdd.push(doubleNumber);
-        } else {
-          numbersToAdd.push(cardNumber[i] * 2);
+          numbersToAdd.push(number[i] * 2);
         }
       } else {
-        numbersToAdd.push(cardNumber[i]);
+        numbersToAdd.push(number[i]);
       }
     }
 
@@ -144,7 +147,7 @@ export default class CreditCardInput extends Component {
     } else {
       this.invalidNumber = true;
       return "Credit card number is invalid";
-    };
+    }
   }
 
   render() {
@@ -154,18 +157,30 @@ export default class CreditCardInput extends Component {
                     cardHolder={this.state.cardHolder}
                     expMonth={this.state.expMonth}
                     expYear={this.state.expYear}/>
-        <div className="error-message">{this.invalidNumber ? this.isNotANumber ? <p className="error-text">Please enter <span className="num-text">numbers</span>only</p> : <p className="error-text">Card Number is Incorrect</p> : this.showMessage ? <p className="no-error-show">Card Number is Correct</p> : <p className="no-error-transparent">Card Number is Correct</p>}</div>
+        <div className="error-message">{this.invalidNumber ? this.isNotANumber ? <p className="error-text">Please enter <span className="num-text">numbers</span> only</p> : <p className="error-text">Card Number is Incorrect</p> : this.showMessage ? <p className="no-error-show">Card Number is Correct</p> : <p className="no-error-transparent">Card Number is Correct</p>}</div>
         <div className="form-container">
           <form className="form">
             <label>Card Number:</label>
             <div className={this.invalidNumber ? "form-group-error" : "form-group"}>
               <i className="fa fa-credit-card"></i>
-              <input className="form-input" id="search-input" name="cardNumber" maxLength={19} onKeyDown={this.onKeyDown} onChange={this.onInputChange} placeholder="4012-8888-8888-1881" value={this.state.cardNumber}/>
+              <input className="form-input"
+                    id="search-input"
+                    name="cardNumber"
+                    maxLength={19}
+                    onKeyDown={this.onKeyDown}
+                    onChange={this.onInputChange}
+                    placeholder="4012-8888-8888-1881"
+                    value={this.state.cardNumber}/>
             </div>
             <label>Card Holder:</label>
             <div className="form-group">
               <i className="fa fa-user"></i>
-              <input className="form-input" type="text" name="cardHolder" onChange={this.onInputChange} placeholder="Your Name" value={this.state.cardHolder}/>
+              <input className="form-input"
+                     type="text" name="cardHolder"
+                     maxLength={24}
+                     onChange={this.onInputChange}
+                     placeholder="Your Name"
+                     value={this.state.cardHolder}/>
             </div>
             <label>Expiration Date:</label>
             <div className="form-select">
